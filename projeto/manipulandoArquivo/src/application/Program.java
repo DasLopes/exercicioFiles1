ed3537e29333a00c4f57cdb08cfce6ca126357dc
daslopes <daslopes@outlook.com.br>
2024-05-07 15:23:45 -0300

    Atividade proposta pelo Prof. Nelio Alves no Curso JavaPOO

---
+++ projeto/manipulandoArquivo/src/application/Program.java
@@ -0,0 +1,59 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		List<Product> product = new ArrayList<>();
+
+		System.out.print("Enter file path: ");
+		String caminhoArquivo = sc.nextLine(); // "d:\\temp\\in.txt";
+
+		File caminho = new File(caminhoArquivo);
+
+		boolean criarPasta = new File(caminhoArquivo + "\\subPasta").mkdir();
+
+		String caminhoNovoArquivo = caminho.getParent() + "\\subPasta\\out.txt";
+		// File arquivo = new File(caminhoNovoArquivo);
+
+		try (BufferedReader br = new BufferedReader(new FileReader(caminhoArquivo))) {
+
+			String linha = br.readLine();
+			while (linha != null) {
+				String[] vect = linha.split(",");
+				product.add(new Product(vect[0], Double.parseDouble(vect[1]), Integer.parseInt(vect[2])));
+				linha = br.readLine();
+			}
+
+			// arquivo.createNewFile();
+			try (BufferedWriter bw = new BufferedWriter(new FileWriter(caminhoNovoArquivo))) {
+				for (Product string : product) {
+					bw.write(string.getName() + ", " + String.format("%.2f", string.total()));
+					bw.newLine();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+
+		sc.close();
+
+	}
+
+}
